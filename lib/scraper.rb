086e9578ba92ee56784401a25a618029625e3f48
Joshua Crum <joshua.crum@gmail.com>
2019-06-01 23:22:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,24 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css("student.card").map do |student|
+      hash = {
+        name:
+        location:
+        profile_url:
+      }
+      student_array << hash
+    end
+    students_array
+  end
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
+
   end
 
 end
-
