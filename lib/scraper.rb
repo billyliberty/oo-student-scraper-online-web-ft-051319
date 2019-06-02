cad6bd6b42ee252eee1798ad274a3768bc5e7652
Joshua Crum <joshua.crum@gmail.com>
2019-06-02 00:24:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,20 +4,26 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student_array = []
     index_page = Nokogiri::HTML(open(index_url))
+    students = []
     index_page.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
-      hash = {
-        name: student.css(".student-name").text,
-        location: student.css(".student-location").text,
-        profile_url: "#{student.attr("href")}"
-      }
-      student_array << hash
+        student_profile_link = "#{student.attr('href')}"
+        def self.scrape_index_page(index_url)
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
     end
-    student_array
+    students
   end
 
+  
   def self.scrape_profile_page(profile_url)
     student_hash = {}
     html = Nokogiri::HTML (open(profile_url))
