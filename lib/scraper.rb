9037f16095370c80a0403533c0ec62f03664abe4
Joshua Crum <joshua.crum@gmail.com>
2019-06-02 00:27:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,38 +4,43 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
+    page = Nokogiri::HTML(open(index_url))
     students = []
-    index_page.css("div.roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-        student_profile_link = "#{student.attr('href')}"
-        def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
-    students = []
-    index_page.css("div.roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-        student_profile_link = "#{student.attr('href')}"
-        student_location = student.css('.student-location').text
-        student_name = student.css('.student-name').text
-        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name,
+                :location => location,
+                :profile_url => profile_url}
+      students << student_info
       end
-    end
     students
-  end
+   end
 
 
   def self.scrape_profile_page(profile_url)
-    student_hash = {}
-    html = Nokogiri::HTML (open(profile_url))
-    html.css("div.social-icon-controller a").each do |student|
-      url = student.attribute ("href")
-        student_hash[:twitter_url] = url if url.include?("twitter")
-        student_hash[:linkedin_url] = url if url.include?("linkedin")
-        student_hash[:github_url] = url if url.include?("github")
-        student_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
-        student_hash[:profile_quote] = html.css("div.profile-quote").text
-        student_hash[:bio] = html.css("div.bio-content p").text
+      page = Nokogiri::HTML(open(profile_url))
+      student = {}
+
+      # student[:profile_quote] = page.css(".profile-quote")
+      # student[:bio] = page.css("div.description-holder p")
+      container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+      container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
       end
-      student_hash
-    end
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("div.description-holder p").text
+      student
   end
+
+end
